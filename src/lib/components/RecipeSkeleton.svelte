e9693fca952121e4c32e652e1477b42883d91f75
YuanLiem <yuan.liem@hotmail.com>
2025-01-21 15:18:32 +0100

    tweak: RecipeSkeleton component

---
+++ src/lib/components/RecipeSkeleton.svelte
@@ -0,0 +1,12 @@
+<div
+	class="flex h-full animate-pulse flex-col overflow-hidden rounded-xl border shadow-md dark:bg-neutral-800 md:flex-row"
+>
+	<div class="h-64 w-full bg-neutral-300 dark:bg-neutral-700 md:w-1/2"></div>
+	<div class="w-full p-4 md:w-1/2">
+		<div class="mt-4 h-2 w-5/6 rounded-lg bg-neutral-300 dark:bg-neutral-500"></div>
+		{#each { length: 4 }}
+			<div class="mt-4 h-2 w-4/5 rounded-lg bg-neutral-300 dark:bg-neutral-500"></div>
+			<div class="mt-4 h-2 w-3/5 rounded-lg bg-neutral-300 dark:bg-neutral-500"></div>
+		{/each}
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
 	import RecipeCard from '$lib/components/RecipeCard.svelte';
-	import { cn } from '$lib/utils';
+	import RecipeSkeleton from '$lib/components/RecipeSkeleton.svelte';
 	let { data }: PageProps = $props();
 </script>
 
@@ -9,18 +9,7 @@
 	<h1 class="mb-8 text-2xl">Let Math.random decide what's on the menu</h1>
 
 	{#await data.recipe}
-		<div
-			class="flex h-full animate-pulse flex-col overflow-hidden rounded-xl border shadow-md dark:bg-neutral-800 md:flex-row"
-		>
-			<div class="h-64 w-full bg-neutral-300 dark:bg-neutral-700 md:w-1/2"></div>
-			<div class="w-full p-4 md:w-1/2">
-				<div class="mt-4 h-2 w-5/6 rounded-lg bg-neutral-300 dark:bg-neutral-500"></div>
-				{#each { length: 4 }}
-					<div class="mt-4 h-2 w-4/5 rounded-lg bg-neutral-300 dark:bg-neutral-500"></div>
-					<div class="mt-4 h-2 w-3/5 rounded-lg bg-neutral-300 dark:bg-neutral-500"></div>
-				{/each}
-			</div>
-		</div>
+		<RecipeSkeleton />
 	{:then recipe}
 		<RecipeCard {recipe} />
 	{/await}
