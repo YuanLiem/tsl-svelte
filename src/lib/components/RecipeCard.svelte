4985b8aa86ec36e1a474567e91cb6e08f2264370
YuanLiem <yuan.liem@hotmail.com>
2025-01-21 15:15:23 +0100

    add: RecipeCard / Skeleton

---
+++ src/lib/components/RecipeCard.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { Recipe } from '$lib/types/types';
+
+	let { recipe }: { recipe: Recipe } = $props();
+	type Feature = {
+		label: string;
+	};
+</script>
+
+{#snippet feature({ ...props }: Feature)}
+	<div class="w-fit rounded-full border">
+		{props.label}
+	</div>
+{/snippet}
+
+<div class="flex flex-col overflow-hidden rounded-xl border shadow-md md:flex-row">
+	<figure class="overflow-hidden md:w-1/2">
+		<img src={recipe.image} class="h-full" alt={recipe.name} />
+	</figure>
+	<div class="flex-flex-col gap-4 p-4 md:w-1/2">
+		<h3 class="text-xl">{recipe.name}</h3>
+		<div class="flex flex-row items-center justify-between">
+			<p class="text-muted-foreground">{recipe.cuisine}</p>
+			{recipe.rating}
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
+	import RecipeCard from '$lib/components/RecipeCard.svelte';
+	import { cn } from '$lib/utils';
 	let { data }: PageProps = $props();
 </script>
 
-<h1 class="text-2xl">Let Math.random decide what's on the menu</h1>
-{#await data.recipe}
-	.. loading recipe
-{:then recipe}
-	<div>
-		<h3 class="text-xl">{recipe.name}</h3>
-		<img src={recipe.image} width="200" alt={recipe.name} />
-		<p class="text-muted-foreground">{recipe.cuisine}</p>
-	</div>
-{/await}
+<div class="container py-8">
+	<h1 class="mb-8 text-2xl">Let Math.random decide what's on the menu</h1>
+
+	{#await data.recipe}
+		<div
+			class="flex h-full animate-pulse flex-col overflow-hidden rounded-xl border shadow-md dark:bg-neutral-800 md:flex-row"
+		>
+			<div class="h-64 w-full bg-neutral-300 dark:bg-neutral-700 md:w-1/2"></div>
+			<div class="w-full p-4 md:w-1/2">
+				<div class="mt-4 h-2 w-5/6 rounded-lg bg-neutral-300 dark:bg-neutral-500"></div>
+				{#each { length: 4 }}
+					<div class="mt-4 h-2 w-4/5 rounded-lg bg-neutral-300 dark:bg-neutral-500"></div>
+					<div class="mt-4 h-2 w-3/5 rounded-lg bg-neutral-300 dark:bg-neutral-500"></div>
+				{/each}
+			</div>
+		</div>
+	{:then recipe}
+		<RecipeCard {recipe} />
+	{/await}
+</div>
