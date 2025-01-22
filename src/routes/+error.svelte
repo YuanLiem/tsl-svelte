bf84a304012d89927ed15c4b08a9cc51cbde3206
YuanLiem <yuan.liem@hotmail.com>
2025-01-22 20:10:26 +0100

    add: Error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { buttonVariants } from '$lib/components/ui/button';
+	import { page } from '$app/state';
+</script>
+
+<div class="container">
+	<div class="h-screen content-center">
+		{#if page?.error?.message}
+			<h1 class="mb-4 text-2xl font-bold">Something went wrong..</h1>
+			<a href="/" class={buttonVariants({ size: 'lg' })}>Go back</a>
+		{/if}
+	</div>
+</div>
