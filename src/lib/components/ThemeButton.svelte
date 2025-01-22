2d903a71c951c4fd4f5398b8dbae35c560ced51d
YuanLiem <yuan.liem@hotmail.com>
2025-01-22 18:26:47 +0100

    add: Mobile Nav
    and tweaked ThemeButton to accept classes

--- src/lib/components/ThemeButton.svelte
+++ src/lib/components/ThemeButton.svelte
@@ -2,9 +2,13 @@
 	import Button from '$lib/components/ui/button/button.svelte';
 	import { Sun, Moon } from 'lucide-svelte';
 	import { toggleMode, mode } from 'mode-watcher';
+	import type { ButtonProps } from '$lib/components/ui/button/button.svelte';
+	import { cn } from '$lib/utils';
+
+	let { ...props }: ButtonProps = $props();
 </script>
 
-<Button class="hidden md:flex" onclick={toggleMode} variant="outline" size="icon">
+<Button class={cn('rounded-full', props.class)} onclick={toggleMode} variant="outline" size="icon">
 	{#if $mode === 'dark'}
 		<Sun class="h-4 w-4" />
 	{:else}
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -5,13 +5,21 @@
 	import type { MenuItem } from '$lib/types/types';
 
 	import { cn } from '$lib/utils';
+	import { Menu, X } from 'lucide-svelte';
+	import { Button } from '$lib/components/ui/button';
+	let isOpen = $state(false);
+
+	function handleKeyDown(e: KeyboardEvent) {
+		if (e.key !== 'Enter' && e.key !== ' ') return;
+		isOpen = !isOpen;
+	}
 </script>
 
 {#snippet menuItem({ ...props }: MenuItem)}
 	<a
 		href={props.slug}
 		class={cn(
-			page.url.pathname === props.slug && 'bg-teal-500 text-white',
+			page.url.pathname === props.slug && 'flex w-fit bg-teal-500 text-white',
 			'rounded-md px-4 py-2 hover:opacity-80'
 		)}
 	>
@@ -21,16 +29,60 @@
 	</a>
 {/snippet}
 
-<header class="sticky top-0 flex items-center justify-between bg-primary-foreground p-4 shadow-md">
+{#snippet mobileItem({ ...props }: MenuItem)}
+	<a
+		href={props.slug}
+		class={cn(
+			page.url.pathname === props.slug && 'bg-teal-500 text-white',
+			'flex w-full rounded-md px-4 py-2 hover:opacity-80'
+		)}
+		onclick={() => (isOpen = !isOpen)}
+	>
+		<li>
+			{props.label}
+		</li>
+	</a>
+{/snippet}
+
+<header
+	class="sticky top-0 z-30 flex items-center justify-between bg-primary-foreground p-4 shadow-md"
+>
 	<div class="font-bold">
 		{siteConfig.companyName}
 	</div>
 	<nav class="flex flex-row items-center gap-4">
-		<ul class="hidden max-w-sm bg-red-500 md:contents">
+		<ul class="hidden max-w-sm md:contents">
 			{#each siteConfig.menuItems as item}
 				{@render menuItem(item)}
 			{/each}
 		</ul>
 	</nav>
-	<ThemeButton />
+	<Button size="icon" variant="outline" class="flex md:hidden" onclick={() => (isOpen = !isOpen)}>
+		{#if isOpen}
+			<X />
+		{:else}
+			<Menu />
+		{/if}
+	</Button>
+	<ThemeButton class="hidden md:flex" />
 </header>
+{#if isOpen}
+	<nav class="fixed top-16 z-10 flex h-full w-full flex-col bg-primary-foreground/60">
+		<ul class="flex flex-col gap-2 bg-primary-foreground px-4 py-8">
+			{#each siteConfig.menuItems as item}
+				{@render mobileItem(item)}
+			{/each}
+			<li class="flex flex-row justify-end">
+				<ThemeButton />
+			</li>
+		</ul>
+		<div
+			role="button"
+			tabindex="0"
+			aria-label="Close menu"
+			onkeydown={handleKeyDown}
+			class="flex-grow"
+			onclick={() => (isOpen = !isOpen)}
+		></div>
+	</nav>
+{/if}
