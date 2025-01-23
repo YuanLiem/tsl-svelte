ae505f8318927af534cba38eed5eb4b747d2646f
YuanLiem <yuan.liem@hotmail.com>
2025-01-23 13:56:34 +0100

    add: Basic url parameter pagination

--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
 	import RecipePreview from '$lib/components/RecipePreview.svelte';
+	import { page } from '$app/state';
 
 	let { data } = $props();
+	let perPage = $state(12);
+	let totalPages = $derived(Number(Math.ceil(data.recipeData.total / perPage)));
+	let currentPage = $derived(Math.floor(Number(page.url.searchParams.get('skip') || 0) / perPage));
 </script>
 
 <div class="container">
 	<div class="flex flex-row">Search bar goes here</div>
 	<div class="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-4">
-		{#await data.recipes}
-			waiting for data..
-		{:then recipes}
-			{#each recipes as recipe}
-				<RecipePreview props={recipe} />
-			{/each}
-		{/await}
+		{#each data.recipeData.recipes as recipe}
+			<RecipePreview props={recipe} />
+		{/each}
 	</div>
-	<div class="flex flex-row">Pagination links go here</div>
+	<div class="mx-auto flex max-w-lg flex-row justify-center gap-10 py-8">
+		{#each { length: totalPages } as _, page}
+			<a
+				href={`/recipes?limit=${perPage}&skip=${perPage * page}`}
+				class={currentPage === page ? 'text-emerald-300 hover:opacity-80' : 'hover:opacity-80'}
+			>
+				{page + 1}
+			</a>
+		{/each}
+	</div>
 </div>
---
+++ src/routes/recipes/+page.ts
@@ -0,0 +1,14 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ url, fetch }) => {
+	const limit = Number(url.searchParams.get('limit') || 12);
+	const skip = Number(url.searchParams.get('skip') || 0);
+	async function getRecipeChunk(limit: number = 10, skip: number = 0) {
+		const request = await fetch(`https://dummyjson.com/recipes?limit=${limit}&skip=${skip}`);
+		const response = await request.json();
+		return response;
+	}
+	return {
+		recipeData: await getRecipeChunk(limit, skip)
+	};
+};
