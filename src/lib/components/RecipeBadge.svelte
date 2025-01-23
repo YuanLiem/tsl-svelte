4fea21118822e0341bc0cc87b1ad9b8326aca68d
YuanLiem <yuan.liem@hotmail.com>
2025-01-23 21:23:37 +0100

    fix: add conditional check for icon to RecipeBadge

--- src/lib/components/RecipeBadge.svelte
+++ src/lib/components/RecipeBadge.svelte
@@ -6,7 +6,9 @@
 </script>
 
 <div class="inline-flex w-fit rounded-full border bg-muted px-2 py-0.5 text-sm">
-	<features.icon class="mr-2 h-4 w-4" />
+	{#if features.icon}
+		<features.icon class="mr-2 h-4 w-4" />
+	{/if}
 	<span class="text-xs">
 		{features.value}
 	</span>
