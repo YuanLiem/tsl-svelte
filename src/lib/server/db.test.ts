d5fedab9d2da4e02880e36e424f0082ed539ab5e
YuanLiem <yuan.liem@hotmail.com>
2025-01-26 22:00:21 +0100

    test: db unit test for checking random index range

---
+++ src/lib/server/db.test.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect } from 'vitest';
+
+describe('db.getRandomRecipe', () => {
+	it('should generate a random index between 1 and 50', async () => {
+		const randomDigits: number[] = [];
+		for (let i = 0; i < 1000; i++) {
+			const randomDigit = Math.floor(Math.random() * 50 + 1);
+			randomDigits.push(randomDigit);
+		}
+
+		//const failedValues = randomDigits.filter((i) => i < 1 || i > 50);
+		//
+		//console.log(`failed values is ${failedValues}`);
+		expect(randomDigits.every((i) => i >= 1 && i <= 50)).toBe(true);
+	});
+});
--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,7 +2,7 @@ import type { Recipe } from '$lib/types/types';
 
 export const db = {
 	getRandomRecipe: async (): Promise<Recipe> => {
-		const randomIndex = Math.floor(Math.random() * (50 - 1));
+		const randomIndex = Math.floor(Math.random() * 50 + 1);
 		const response = await fetch(`https://dummyjson.com/recipes/${randomIndex}`);
 		const data = await response.json();
 
