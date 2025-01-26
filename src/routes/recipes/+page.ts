11eef3e4c0a2a4b3a0ea7aec5c649c099612ad8f
YuanLiem <yuan.liem@hotmail.com>
2025-01-26 22:18:16 +0100

    add: boundary check to api call

--- src/routes/recipes/+page.ts
+++ src/routes/recipes/+page.ts
@@ -3,7 +3,9 @@ import type { PageLoad } from './$types';
 export const load: PageLoad = async ({ url, fetch }) => {
 	const limit = Number(url.searchParams.get('limit') || 12);
 	const skip = Number(url.searchParams.get('skip') || 0);
+
 	async function getRecipeChunk(limit: number = 10, skip: number = 0) {
+		if (skip > 49) return;
 		const request = await fetch(`https://dummyjson.com/recipes?limit=${limit}&skip=${skip}`);
 		const response = await request.json();
 		return response;
